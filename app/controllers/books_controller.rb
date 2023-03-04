d889a07c92d6387471feaa81768772c7297ef080
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-03-04 13:07:05 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
+before_action :authenticate_user!
+before_action :ensure_correct_user, only: [:update, :edit, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
@@ -35,13 +38,20 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    @book.delete
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+   unless @book.user == current_user
+   redirect_to books_path
+   end
   end
 end
